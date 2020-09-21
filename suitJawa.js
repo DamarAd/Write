271aed31017fb8b62484a2cfc8ba35b51995e49f
DamarAd <damaradyun@gmail.com>
2020-09-21 09:44:45 +0700

    finish gim suit jawa

--- suitJawa.js
+++ suitJawa.js
@@ -1,43 +1,49 @@
-// menangkap pilihan player
-var player = prompt('Mana yang anda pilih? jempol / telunjuk / kelingking');
+var tanya = true;
 
-// menangkap pilihan computer
-// membangkitkan bilangan random
-var comp = Math.random();
+while ( tanya ) {
+    
+    // menangkap pilihan player
+    var player = prompt('Mana yang anda pilih? jempol / telunjuk / kelingking');
 
-if ( comp < 0.34 ) {
-    comp = 'jempol';    
-} else if ( comp >= 0.3 && comp < 0.67 ) {
-    comp = 'telunjuk';
-} else {
-    comp = 'kelingking';
+    // menangkap pilihan computer
+    // membangkitkan bilangan random
+    var comp = Math.random();
+
+    if ( comp < 0.34 ) {
+        comp = 'jempol';    
+    } else if ( comp >= 0.3 && comp < 0.67 ) {
+        comp = 'telunjuk';
+    } else {
+        comp = 'kelingking';
+    }
+
+
+    var hasil = '';
+    // menentukan rules
+    if ( player == comp ) {
+        hasil = 'SERI!';
+    } else if ( player == 'jempol' ) {
+        // if ( comp == 'telunjuk' ) {
+        //     hasil = 'MENANG!';
+        // } else {
+        //     hasil = 'KALAH!';
+        // }
+        hasil = ( comp == 'telunjuk' ) ? 'MENANG!' : 'KALAH';
+    } else if ( player == 'telunjuk' ) {
+        hasil = ( comp == 'kelingking' ) ? 'MENANG!' : 'KALAH!';
+    } else if ( player == 'kelingking' ) {
+        hasil = ( comp == 'jempol' ) ? 'MENANG!' : 'KALAH!';
+    } else {
+        hasil = 'Anda memasukkan pilihan yang salah!';
+    }
+
+    // tampilkan hasilnya
+    alert('Kamu memilih ' + player + ' dan Komputer memilih ' + comp + '\nMaka hasilnya : Kamu ' + hasil );
+
+    tanya = confirm('Mau main lagi?');
 }
 
-
-var hasil = '';
-// menentukan rules
-if ( player == comp ) {
-    hasil = 'SERI!';
-} else if ( player == 'jempol' ) {
-    // if ( comp == 'telunjuk' ) {
-    //     hasil = 'MENANG!';
-    // } else {
-    //     hasil = 'KALAH!';
-    // }
-    hasil = ( comp == 'telunjuk' ) ? 'MENANG!' : 'KALAH';
-} else if ( player == 'telunjuk' ) {
-    hasil = ( comp == 'kelingking' ) ? 'MENANG!' : 'KALAH!';
-} else if ( player == 'kelingking' ) {
-    hasil = ( comp == 'jempol' ) ? 'MENANG!' : 'KALAH!';
-} else {
-    hasil = 'Anda memasukkan pilihan yang salah!';
-}
-
-// tampilkan hasilnya
-alert('Kamu memilih ' + player + ' dan Komputer memilih ' + comp + '\nMaka hasilnya : Kamu ' + hasil );
-
-
-
+alert('Terima kasih sudah bermain.');
 
 
 
